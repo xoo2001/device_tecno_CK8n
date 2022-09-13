898ab3abc9ffdfe40d8a8bedaab650af6c40d4b0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-13 19:08:10 +0100

    X6739: Enable vendor RIL service
    * We use Mediatek's RIL implementation
    
    Change-Id: Iaab09d72110fdc3887fda6f543b988bdb3e98a4c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,5 +106,8 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
 # Inherit the proprietary files
 include vendor/infinix/X6739/BoardConfigVendor.mk
