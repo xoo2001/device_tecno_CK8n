a8b424290ea80183524e537f5dcb01cf7cb41eab
Sal Savage <salsavage@google.com>
2022-10-01 13:57:30 +0100

    X6739: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values: 90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is correct
    
    Change-Id: I24fd57bacbf6786a26f7079e7a6e9857dbe170d1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,7 @@ BOARD_HAS_MTK_HARDWARE := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
