d8bddefee372fb469489b3066817e58590c49744
Woomymy <woomy@woomy.be>
2023-03-31 18:52:07 +0200

    X6739: Enable userspace reboots
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I94bf051477e4dd84660603bb598fefa4fb384ed4
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Allow userspace reboots
+$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
+
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
