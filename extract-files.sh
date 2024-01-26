c3a419ccaa5d36dff5f0548dbcb6dcb22c6f374a
AndroidHQ254 <d.murimi14@gmail.com>
2024-01-26 04:11:22 +0300

    X6739: Switch to ndk backend for vibrator service
    grep -r "android.hardware.vibrator-V2-ndk_platform.so"
    
    grep: system_ext/apex/com.android.vndk.v31.apex: binary file matches
    grep: vendor/bin/hw/android.hardware.vibrator-service.mediatek: binary file matches
    
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.vibrator-service.mediatek": library "android.hardware.vibrator-V2-ndk_platform.so" not found: needed by main executable

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,9 @@ function blob_fixup() {
         vendor/bin/hw/android.hardware.gnss-service.mediatek | vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
             "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
             ;;
+        vendor/bin/hw/android.hardware.vibrator-service.mediatek)
+            "$PATCHELF" --replace-needed "android.hardware.vibrator-V2-ndk_platform.so" "android.hardware.vibrator-V2-ndk.so" "$2"
+            ;;
         vendor/bin/hw/camerahalserver)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v31.so" "${2}"
             "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v31.so" "${2}"
