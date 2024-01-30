34fcbaca5d2cb8045fdcc4e79250f2da9ac7dd0a
Woomymy <woomy@woomy.be>
2024-01-30 04:55:00 +0300

    X6739: Use 64-bits C2 service
    * Works as fine as 32-bits one
    
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I7bc9ae678af2acb2c515bbb6b8622af3118302a7

--- extract-files.sh
+++ extract-files.sh
@@ -77,6 +77,9 @@ function blob_fixup() {
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v31.so" "${2}"
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v31.so" "${2}"
             ;;
+        vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc)
+            sed -i 's/@1.2-mediatek/@1.2-mediatek-64b/g' "${2}"
+            ;;
         vendor/lib/hw/audio.primary.mt6893.so)
             ;&
         vendor/lib64/hw/audio.primary.mt6893.so)
