018ad4ea78493f9df7044bb9c13c239fcd0876df
AndroidHQ254 <d.murimi14@gmail.com>
2024-01-30 06:28:27 +0300

    X6739: Import needed PQ configs
    E PQXmlParser: load cust_color.xml fail
    
    * Fixes HDR video playback on apps like YouTube

--- device.mk
+++ device.mk
@@ -255,6 +255,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Picture Quality
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/pq,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek \
