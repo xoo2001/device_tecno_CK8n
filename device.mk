9254e637e32c9130bc238d1714581b880a609cd9
ziadtamer11 <ziadtamer150@gmail.com>
2024-02-06 20:07:33 +0200

    X6739: Switch to AIDL pixel thermal service
    * Config based off "dumpsys thermalservice" using stock thermal service.
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I5d795d97045651a44e5e9149af76746970e4be83

--- device.mk
+++ device.mk
@@ -411,12 +411,16 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/mediatek
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@2.0.vendor
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # USB
 $(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
