ee2d8a4eac0ff2470d5025081121ea4aa63a76ab
Saikrishna1504 <saikrishna26918@gmail.com>
2024-12-18 14:41:38 +0530

    X6739: Switch to common MediaTek Health AIDL
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -179,9 +179,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service \
+    android.hardware.health-service.mediatek \
     libhidlmemory.vendor
 
 # HIDL
