19a5165eca9c835f125e2e744eb6cddc1fbb6326
gianogli <gianogli@tiscali.it>
2025-04-21 03:25:20 +0200

    CK8n: Drop manual copy of libstagefright_foundation-v33.so
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- device.mk
+++ device.mk
@@ -476,10 +476,6 @@ PRODUCT_PACKAGES += \
     libhidlbase-v31 \
     libutils-v31
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm64/arch-arm-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
-
 PRODUCT_PACKAGES += \
     libunwindstack.vendor \
     libutilscallstack.vendor
