65705d7ed33b3b930dd5beed1858c75b70e81d3c
Edwin Moquete <edwinmmoquete@gmail.com>
2025-05-14 00:13:26 +0800

    CK8n: Move DT2W handling to powerhint
    Shirayuki39: Also drop powerhint extension
    
    Change-Id: Icd035962d289996feb76ddeaf02bf1843bd9d64a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,10 +133,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Partitions - Metadata
 BOARD_USES_METADATA_PARTITION := true
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/gesture_function"
-TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(DEVICE_PATH):libperfmgr-ext-transsion
-
 # Platform
 TARGET_BOARD_PLATFORM := mt6893
 BOARD_HAS_MTK_HARDWARE := true
